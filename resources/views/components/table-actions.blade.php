9e436f5e30609012a26eaadc26fd36575ddfb0c5
gustavo.lanzas <gustavo.lanzas@gmail.com>
2025-05-12 16:56:48 -0700

    refactor: make delete button consistent with edit button for locked records
    Update table-actions.blade.php to wrap both edit and delete buttons in the same lock-checking logic. Both buttons should follow the same behavior for locked records.
    
    ```git-revs
    0376bfe  (Base revision)
    HEAD     Update table-actions to make delete button consistent with edit button for locked records
    ```
    
    codemcp-id: 84-refactor-make-delete-button-consistent-with-edit-b

--- resources/views/components/table-actions.blade.php
+++ resources/views/components/table-actions.blade.php
@@ -8,25 +8,36 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"/>
             </svg>
         </button>
-    @elseif($canEdit)
-        <a href="{{ $editRoute ? route($editRoute, [$routeKeyName => $id]) : '#' }}"
-           class="inline-flex items-center p-2 text-xs bg-emerald-100 text-emerald-700 rounded-lg hover:bg-emerald-200">
-            <x-heroicon-o-pencil-square class="{{ $size }}" />
-        </a>
-    @else
-        {{-- Disabled edit button --}}
-        <button disabled
-                title="This record is currently being edited by another user"
-                class="inline-flex items-center p-2 text-xs bg-gray-100 text-gray-400 rounded-lg cursor-not-allowed">
-            <x-heroicon-o-pencil-square class="{{ $size }}" />
-        </button>
-    @endif
 
-    @if($canDelete)
-        <button
-            wire:click="dispatch('{{ $deleteEvent }}', { id: {{ $id }} })"
-            class="inline-flex items-center p-2 text-xs bg-red-100 text-red-700 rounded-lg hover:bg-red-200">
+        {{-- Disabled delete button for locked records --}}
+        <button disabled
+                title="{{ $lockerName }} is currently editing this request"
+                class="inline-flex items-center p-2 text-xs bg-gray-100 text-gray-400 rounded-lg cursor-not-allowed">
             <x-heroicon-o-trash class="{{ $size }}" />
         </button>
+    @else
+        {{-- Edit button --}}
+        @if($canEdit)
+            <a href="{{ $editRoute ? route($editRoute, [$routeKeyName => $id]) : '#' }}"
+               class="inline-flex items-center p-2 text-xs bg-emerald-100 text-emerald-700 rounded-lg hover:bg-emerald-200">
+                <x-heroicon-o-pencil-square class="{{ $size }}" />
+            </a>
+        @else
+            {{-- Disabled edit button --}}
+            <button disabled
+                    title="This record is currently being edited by another user"
+                    class="inline-flex items-center p-2 text-xs bg-gray-100 text-gray-400 rounded-lg cursor-not-allowed">
+                <x-heroicon-o-pencil-square class="{{ $size }}" />
+            </button>
+        @endif
+
+        {{-- Delete button --}}
+        @if($canDelete)
+            <button
+                wire:click="dispatch('{{ $deleteEvent }}', { id: {{ $id }} })"
+                class="inline-flex items-center p-2 text-xs bg-red-100 text-red-700 rounded-lg hover:bg-red-200">
+                <x-heroicon-o-trash class="{{ $size }}" />
+            </button>
+        @endif
     @endif
 </div>
