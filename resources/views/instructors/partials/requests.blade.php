a0a28bbb8c00e65d9d2f073dab94198063feb97f
gustavo.lanzas <gustavo.lanzas@gmail.com>
2025-05-19 10:42:18 -0700

    instructor table

--- resources/views/instructors/partials/requests.blade.php
+++ resources/views/instructors/partials/requests.blade.php
@@ -4,7 +4,7 @@
 @include('instruction-requests.partials.lock-urls')
 
 <div class="mt-6">
-    <h2 class="text-lg font-medium text-gray-900 dark:text-white mb-2">Instruction Requests</h2>
+    <h2 class="text-lg font-medium text-gray-900 dark:text-white mb-2">Instruction Requests by this instructor</h2>
     <div class="table-responsive">
         <livewire:instructor-requests-table :instructorId="$instructor->id" />
     </div>
